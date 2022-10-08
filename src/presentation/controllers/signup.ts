813fb56514394b9c0f6258e381ff5df5ece60fb4
Nataniel Costa <nataniel08@protonmail.com>
2022-10-08 17:52:28 -0300

    feat: ensure SignUpController returns 400 fi no password confirmatio is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,7 +4,7 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController {
 	handle (httpRquest: HttpRequest): HttpResponse {
-		const requiredFilds = ['name', 'email', 'password']
+		const requiredFilds = ['name', 'email', 'password', 'passwordConfirmation']
 		for (const field of requiredFilds) {
 			if (!httpRquest.body[field]) {
 				return badRequest(new MissingParamError(field))
